9c0d6aaf330fd4292b0ca61b7b3fecb6e2f57f9e
Sean Rea <seanrea93@gmail.com>
2023-01-22 21:34:19 -0600

    started working on client and server communication.
    Puled the socketserver examples from docs and dropped them
    into the prototypes dir. Worked on implementing them into the
    server and client classes.

--- sean/python/chatroom.py
+++ sean/python/chatroom.py
@@ -8,9 +8,11 @@ They want to be able to go to a website. Search for each other's name and initia
 * Any one can initiate the close of communication and the end and lost of the chat.
 
 * clients can leave chat with out closing the chat, if they choose.
-
-
 '''
+import socketserver
+import socket
+import sys
+
 class SessionManager():
     def __init__(self, id):
         self.session_list = []
@@ -25,14 +27,49 @@ class SessionManager():
     def destroy_session(self, session_id):
         self.session_list.remove(session_id)
 
+'''
+
+'''
+class ConnectionHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+        # just send back the same data
+        self.request.sendall(self.data)
+
 class Session():
     def __init__(self, session_id):
         self.client_list = []
         self.session_id = session_id
-        pass
+        self.server_address = "localhost"
+        self.server_port = "15555"
 
     def __str__(self):
         return ' '.join(map(str,self.client_list))
+
+    def start_session(self):
+        '''
+        Setup a tcp server and start listening for clients
+        Things to add eventually
+        * tls setup 
+        '''
+        HOST, PORT = "localhost", 9999
+
+        # Create the server, binding to localhost on port 9999
+        with socketserver.TCPServer((HOST, PORT),ConnectionHandler) as server:
+            # Activate the server; this will keep running until you
+            # interrupt the program with Ctrl-C
+            server.serve_forever()
 
     def attach_client(self, client):
         self.client_list.append(client)
@@ -43,18 +80,54 @@ class Session():
 class Client():
     def __init__(self, client_id):
         self.client_id = client_id
-        pass
 
     def __str__(self):
         return str(self.client_id)
+        
+    def client_test(self):
+        HOST, PORT = "localhost", 9999
+        data = " ".join(self.client_id)
+    
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            sock.connect((HOST, PORT))
+            sock.sendall(bytes(data + "\n", "utf-8"))
+    
+            # Receive data from the server and shut down
+            received = str(sock.recv(1024), "utf-8")
+    
+        print("Sent:     {}".format(data))
+        print("Received: {}".format(received))
+    #def receive_messages(client):
+    #    while True:
+    #        try:
+    #            # Receive a message from the server
+    #            message = client.recv(1024)
+    
+    #            # If the message is empty, the server has disconnected
+    #            if not message:
+    #                print("Server disconnected")
+    #                client.close()
+    #                break
+    #               
+    #            # Decrypt the message and print it
+    #            message = message.decode("utf-8")
+    #            print(message)
+    #        except:
+    #            # If there is an error, the server has disconnected
+    #            print("Server disconnected")
+    #            client.close()
+    #            break
 
 def main():
     pass
 #    client_1 = Client("1234")
 #    client_2 = Client("1235")
-#    session_1 = Session()
 #    session_1.attach_client(client_1)
 #    session_1.attach_client(client_2)
+    session_1 = Session("4321")
+    session_1.start_session()
 
 if __name__ == "__main__":
     main()
